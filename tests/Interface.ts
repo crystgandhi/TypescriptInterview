0b598d27b85c40b785ffdcb7ac394dedff5c6fd1
crystgandhi <105329072+crystgandhi@users.noreply.github.com>
2026-06-30 19:12:24 -0400

    Interface

--- tests/Interface.ts
+++ tests/Interface.ts
@@ -1,3 +1,14 @@
-interface Bank{
-
+interface Bank {
+    bankName: string
+    calculateInterest(): number;
 }
+class SBI implements Bank {
+    constructor(public bankName: string, public amount: number) { }
+    calculateInterest(): number {
+        return this.amount * 0.05;
+    }
+}
+
+const sbi = new SBI("SBI", 98690);
+console.log(sbi.calculateInterest());
+
